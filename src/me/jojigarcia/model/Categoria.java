c560651f0f054857d59be7339642f4680830ad2a
joaquinjimenezgarcia <garcia.jjimenez@gmail.com>
2017-05-10 18:43:02 +0200

    - Hecho Enum "Categoria"

--- src/me/jojigarcia/model/Categoria.java
+++ src/me/jojigarcia/model/Categoria.java
@@ -4,4 +4,17 @@ package me.jojigarcia.model;
  * Created by joaquinjimenezgarcia on 10/5/17.
  */
 public enum Categoria {
+    INFORMATICA("Informática"),
+    ELECTRONICA("Electrónica"),
+    VIDEOJUEGOS("Videojuegos");
+
+    private final String categoria;
+
+    Categoria(String categoria){
+        this.categoria = categoria;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
 }
