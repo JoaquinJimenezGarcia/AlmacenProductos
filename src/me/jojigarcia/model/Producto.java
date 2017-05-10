d6f888c454e92995b42dc205543623023be92b77
joaquinjimenezgarcia <garcia.jjimenez@gmail.com>
2017-05-10 18:40:06 +0200

    - Hecha clase "Producto"

--- src/me/jojigarcia/model/Producto.java
+++ src/me/jojigarcia/model/Producto.java
@@ -4,4 +4,76 @@ package me.jojigarcia.model;
  * Created by joaquinjimenezgarcia on 10/5/17.
  */
 public class Producto {
+    String nombre;
+    Categoria categoria;
+    double peso;
+    double volumen;
+
+    public Producto() {
+        this.nombre = "Desconocido";
+        this.categoria = categoria;
+        this.peso = 0;
+        this.volumen = 0;
+    }
+
+    public Producto(String nombre, Categoria categoria, double peso, double volumen) {
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.peso = peso;
+        this.volumen = volumen;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        if (nombre.equals("")){
+            this.nombre = "Desconocido";
+        }else {
+            this.nombre = nombre;
+        }
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        if (this.peso < 0){
+            this.peso = 0;
+        }else {
+            this.peso = peso;
+        }
+    }
+
+    public double getVolumen() {
+        return volumen;
+    }
+
+    public void setVolumen(double volumen) {
+        if (this.volumen < 0){
+            this.volumen = 0;
+        }else {
+            this.volumen = volumen;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Producto( " +
+                "nombre = " + nombre +
+                ", categoria = " + categoria +
+                ", peso = " + peso +
+                ", volumen = " + volumen +
+                " )";
+    }
 }
